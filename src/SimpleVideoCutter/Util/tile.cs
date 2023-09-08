26ce705f23f20c32d12c822c77fced2087ebc12b
DaveCS1 <daveshipman@gmail.com>
2023-09-08 17:44:25 -0500

    collage

--- src/SimpleVideoCutter/Util/tile.cs
+++ src/SimpleVideoCutter/Util/tile.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace SimpleVideoCutter.Util
 {
   public  class tile
@@ -21,15 +21,29 @@ namespace SimpleVideoCutter.Util
         private void ConvertButton_Click()
         {
             // Input video file path
-            string inputFilePath = @"C:\path\to\input.mp4";
+            //string inputFilePath = @"C:\path\to\input.mp4";
 
-            // Output video file path
-            string outputFilePath = @"C:\path\to\output.mp4";
+            //// Output video file path
+            //string outputFilePath = @"C:\path\to\output.mp4";
+            if (File.Exists(testOutput))
+            {
+                File.Delete(testOutput);
+            }
 
-            // FFmpeg command
-            //  string ffmpegCommand = $"-i \"{inputFilePath}\" \"{outputFilePath}\"";
-            //-loglevel panic -y -i \"2.mp4\" -frames 1 -q:v 1 -vf \"select=not(mod(n\\,4)),scale=180:140,tile=6x6\" video_preview1_new9_8a.jpg
-            string ffmpegCommand = $" -loglevel panic -y -i \"{testMediaPath}\" -frames 1 -q:v 1 -vf \"select=not(mod(n\\,4)),scale=180:140,tile=6x6\"  \"{testOutput}\"";
+            //https://stackoverflow.com/questions/58188556/what-is-select-notmodn-500-doing-in-ffmpeg
+
+            /*1
+
+ In select=not(mod(n\,500)),
+
+ select invokes the select video filter, which sends forward a frame if expression evaluates to non-zero, else discards it.
+
+ mod(var,X) returns the modulus (remainder after division) of dividing var by X. In mod(n,500), n is the current frame's index, starting from zero, so this expression will evaluate to 0, 1, 2 ... 498, 499, 0, 1, 2, ... 498, 499, 0, 1, 2... for increasing n.
+
+ not(expr) inverts the value of the expression contained inside the brackets - 0 if expr is non-zero and 1 if expr evaluates to zero. */
+
+
+            string ffmpegCommand = $" -loglevel panic -y -i \"{testMediaPath}\" -frames 1 -q:v 1 -vf \"select=not(mod(n\\,100)),scale=220:160,tile=4x4\"  \"{testOutput}\"";
 
             // Create a new process start info
             ProcessStartInfo startInfo = new ProcessStartInfo
