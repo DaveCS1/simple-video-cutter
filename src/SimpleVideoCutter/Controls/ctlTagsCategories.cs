8f6c77e36a5724d110c11a6d125a8c4068aae6f2
DaveCS1 <daveshipman@gmail.com>
2023-09-13 16:14:02 -0500

    updating tags

--- src/SimpleVideoCutter/Controls/ctlTagsCategories.cs
+++ src/SimpleVideoCutter/Controls/ctlTagsCategories.cs
@@ -22,62 +22,23 @@ namespace SimpleVideoCutter.Controls
         }
 
 
-        
+        private  DatabaseHelper db;
         private void ctlTagsCategories_Load(object sender, EventArgs e)
         {
             //List<string> tags = new List<string>() { "tag", "tag1", "tag2", "tag3", "tag4", "tag5", "tag6", "tag7", };
             radCheckedDropDownList1.DataSource = GetAllTags();
             //GetAllTags();
+            db = new DatabaseHelper($"Data Source={VideoCutterSettings.DatabasePath};Version=3;");
 
-        }
-
-        //cmd.CommandText = "INSERT INTO wordlist (word) 
-        //           SELECT('word')
-        //           WHERE NOT EXISTS
-        //               (SELECT 1 FROM wordlist WHERE word = 'word');";
-
-
-
-        //Add tags
-        static void AddTagList(List<string> tagListFromAddTagsTextBox)
-        {
-           
-            string dbFilePath = VideoCutterSettings.DatabasePath;
                       
-            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dbFilePath};Version=3;"))
-            {
-                connection.Open();
-
-                foreach (string tagValue in tagListFromAddTagsTextBox)
-                {
-                    using (SQLiteCommand command = connection.CreateCommand())
-                    {                     
-                        command.Parameters.AddWithValue("@Tag", tagValue);
-
-                        command.CommandText = "SELECT count(*) FROM Tags WHERE Tag=@Tag";
-                        int count = Convert.ToInt32(command.ExecuteScalar());
-                        if (count == 0)
-                        {
-                            command.CommandText = "INSERT INTO Tags  (Tag) VALUES (@Tag)";
-                            try
-                            {
-                                command.ExecuteNonQuery();
-                                Console.WriteLine($"Inserted tag: {tagValue}");
-                            }
-                            catch (Exception ex)
-                            {
-                                Console.WriteLine($"Error inserting tag: {ex.Message}");
-                            }
-
-                        }
-         
-                    }
-                }
-
-                connection.Close();
-            }
         }
-        //updated
+
+    
+
+      /// <summary>
+      /// Add Tag(s) accepts list of strings which is a comma seperated list of strings, the tags.
+      /// </summary>
+      /// <param name="tagListFromAddTagsTextBox"></param>
         static void AddTagList1(List<string> tagListFromAddTagsTextBox)
         {
             string dbFilePath = VideoCutterSettings.DatabasePath;
@@ -123,53 +84,121 @@ namespace SimpleVideoCutter.Controls
 
 
 
+        //public List<Tag> GetAllTags()
+
+        //{
+        //    using (SQLiteConnection cn = new SQLiteConnection("Data Source=" + VideoCutterSettings.DatabasePath + "; Version=3"))
+        //    {
+
+        //        cn.Open();
+        //        var cmd = new SQLiteCommand(cn);
+
+        //        cmd.CommandText = "Select * From Tags";
+
+        //        using (SQLiteDataReader rdr = cmd.ExecuteReader())
+        //        {
+        //            try
+        //            {
+        //                var list = new List<Tag>();
+
+        //                while (rdr.Read())
+        //                {
+        //                    if (!rdr.IsDBNull(1))
+        //                    {
+        //                        list.Add(new Tag
+        //                        {
+        //                            Id = rdr.GetInt32(0),
+        //                            TagValue = rdr.GetString(1)
+
+        //                        });
+
+        //                    }
+
+        //                }
+
+        //                return list;
+
+        //            }
+        //            catch (Exception ex)
+        //            {
+
+        //                Console.WriteLine(ex.Message + ex.StackTrace);
+        //                return null;
+        //            }
+
+
+        //        }
+
+        //    }
+
+        //}
+        //public List<Tag> GetAllTags()
+        //{
+        //    string connectionString = "Data Source=" + VideoCutterSettings.DatabasePath + "; Version=3";
+
+        //    using (SQLiteConnection cn = new SQLiteConnection(connectionString))
+        //    {
+        //        cn.Open();
+
+        //        using (SQLiteCommand cmd = cn.CreateCommand())
+        //        {
+        //            cmd.CommandText = "SELECT Id, TagValue FROM Tags WHERE TagValue IS NOT NULL";
+
+        //            using (SQLiteDataReader rdr = cmd.ExecuteReader())
+        //            {
+        //                var tags = new List<Tag>();
+
+        //                while (rdr.Read())
+        //                {
+        //                    tags.Add(new Tag
+        //                    {
+        //                        Id = rdr.GetInt32(0),
+        //                        TagValue = rdr.GetString(1)
+        //                    });
+        //                }
+
+        //                return tags;
+        //            }
+        //        }
+        //    }
+        //}
         public List<Tag> GetAllTags()
-
         {
-            using (SQLiteConnection cn = new SQLiteConnection("Data Source=" + VideoCutterSettings.DatabasePath + "; Version=3"))
+            try
             {
-
-                cn.Open();
-                var cmd = new SQLiteCommand(cn);
-
-                cmd.CommandText = "Select * From Tags";
-
-                using (SQLiteDataReader rdr = cmd.ExecuteReader())
+                using (SQLiteConnection cn = new SQLiteConnection("Data Source=" + VideoCutterSettings.DatabasePath + "; Version=3"))
                 {
-                    try
+                    cn.Open();
+
+                    using (SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM Tags", cn))
                     {
-                        var list = new List<Tag>();
-
-                        while (rdr.Read())
+                        using (SQLiteDataReader rdr = cmd.ExecuteReader())
                         {
-                            if (!rdr.IsDBNull(1))
+                            var list = new List<Tag>();
+
+                            while (rdr.Read())
                             {
-                                list.Add(new Tag
+                                if (!rdr.IsDBNull(1))
                                 {
-                                    Id = rdr.GetInt32(0),
-                                    TagValue = rdr.GetString(1)
-
-                                });
-
+                                    list.Add(new Tag
+                                    {
+                                        Id = rdr.GetInt32(0),
+                                        TagValue = rdr.GetString(1)
+                                    });
+                                }
                             }
 
+                            return list;
                         }
-
-                        return list;
-
                     }
-                    catch (Exception ex)
-                    {
-
-                        Console.WriteLine(ex.Message + ex.StackTrace);
-                        return null;
-                    }
-
-
                 }
-
             }
-
+            catch (Exception ex)
+            {
+                // Handle the exception appropriately, e.g., throw, log, or return an empty list
+               
+                throw new Exception("Error fetching tags: " + ex.Message, ex);
+            }
         }
 
         private void radBtnAddTag_Click(object sender, EventArgs e)
@@ -183,30 +212,12 @@ namespace SimpleVideoCutter.Controls
         {
             if (radTextBoxCtlAddTags.Text.Length>0)
                 {
-                    //string tags = radTextBoxCtlAddTags.Text.Trim();
-                    // string[] subs = s.Split(' ');
-                    // string[] tagList = tags.Split(","); 
-                    string s = radTextBoxCtlAddTags.Text.Trim(); //tags;
-                    string[] values = s.Split(',').Select(sValue => sValue.Trim()).ToArray();
+                  
+                //    string s = radTextBoxCtlAddTags.Text.Trim(); //tags;
+                //    string[] values = s.Split(',').Select(sValue => sValue.Trim()).ToArray();
 
-                // string[] values = commaSeparatedText.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-
-                // Create a List<string> from the array
-             //   List<string> resultList = new List<string>(values);
-
-
-                List<string> list = new List<string>(values.ToList());
-                // var school = new
-                //{
-                //    Address = "Orlando",
-                //    Contact = 1200,
-                //    Employee = new { Id = 3, Name = "Tina" }
-                //};
-
-               // List<string> stringList = radTextBoxCtlAddTags.Text
-               //.Split(',')
-               //.Select(tags => tags.Trim()) // Remove leading and trailing spaces
-               //.ToList();
+                //List<string> list = new List<string>(values.ToList());
+                           
 
                 // Split the input text by commas and remove empty entries using LINQ
                 List<string> tagsFromTextBox = radTextBoxCtlAddTags.Text
@@ -231,8 +242,85 @@ namespace SimpleVideoCutter.Controls
 
             }
         }
+        /// <summary>
+        /// Updates Tag  UpdateTag("NewTag", 1); new tag value and Id as parameters
+        /// </summary>
+        /// <param name="newTag"></param>
+        /// <param name="id"></param>
+        public void UpdateTag(string newTag, int id)
+        {
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection("Data Source=" + VideoCutterSettings.DatabasePath + "; Version=3"))
+                {
+                    connection.Open();
 
+                    using (SQLiteCommand command = connection.CreateCommand())
+                    {
+                        command.CommandText = "UPDATE Tags SET Tag = @NewTag WHERE Id = @Id";
+                        command.Parameters.AddWithValue("@NewTag", newTag);
+                        command.Parameters.AddWithValue("@Id", id);
 
+                        int rowsUpdated = command.ExecuteNonQuery();
+
+                        if (rowsUpdated > 0)
+                        {
+                            Console.WriteLine($"Updated person with ID {id} to '{newTag}'.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"No records updated for ID {id}.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception appropriately, e.g., throw, log, or display an error message
+                Console.WriteLine("Error updating tag: " + newTag + id + + ex.Message);
+            }
+        }
+
+        //
+
+        /// <summary>
+        /// Delete a tag by tag name
+        /// </summary>
+        /// <param name="tag"></param>
+        public void DeleteItemByTag(string tag)
+        {
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection("Data Source=" + VideoCutterSettings.DatabasePath + "; Version=3"))
+                {
+                    connection.Open();
+
+                    using (SQLiteCommand command = connection.CreateCommand())
+                    {
+                        command.CommandText = "DELETE FROM Tags WHERE Tag = @Tag";
+                        command.Parameters.AddWithValue("@Tag", tag);
+
+                        int rowsDeleted = command.ExecuteNonQuery();
+
+                        if (rowsDeleted > 0)
+                        {
+                            Console.WriteLine($"Deleted item with tag '{tag}'.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"No records deleted for tag '{tag}'.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception appropriately, e.g., throw, log, or display an error message
+                Console.WriteLine("Error deleting item: " + tag + ex.Message);
+            }
+        }
+
+        //
         //
     }
 }
